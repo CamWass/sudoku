d514816afcac09cc9afd758d2199346d562a8b56
Campbell <campbell.wass@gmail.com>
2023-12-15 12:55:46 +1300

    Add another puzzle

--- src/puzzles.rs
+++ src/puzzles.rs
@@ -1,9 +1,39 @@
-pub static PUZZLES: &'static [(&'static [u8; 81], &'static [u8; 81])] =
-    &[(&INPUT1, &OUTPUT1), (&INPUT2, &OUTPUT2)];
+// pub static PUZZLES: &'static [(&'static [u8; 81], &'static [u8; 81])] =
+//     &[(&INPUT1, &OUTPUT1), (&INPUT2, &OUTPUT2)];
+
+// From Lovatts sudoku book (issue 232, puzzle #110, 'einstein' difficulty).
+#[rustfmt::skip] 
+pub static INPUT3: [u8; 81] = [
+    0,0,0, 0,1,0, 0,0,0,
+    9,0,7, 0,0,0, 5,0,0,
+    0,5,0, 4,0,0, 6,7,0,
+
+    0,8,0, 0,9,0, 3,0,0,
+    0,4,0, 5,0,3, 0,8,0,
+    0,0,3, 0,4,0, 0,6,0,
+
+    0,2,6, 0,0,1, 0,9,0,
+    0,0,5, 0,0,0, 8,0,2,
+    0,0,0, 0,3,0, 0,0,0,
+];
+#[rustfmt::skip]
+pub static OUTPUT3: [u8; 81]  = [
+    8,6,4, 7,1,5, 2,3,9,
+    9,1,7, 3,6,2, 5,4,8,
+    3,5,2, 4,8,9, 6,7,1,
+
+    5,8,1, 6,9,7, 3,2,4,
+    6,4,9, 5,2,3, 1,8,7,
+    2,7,3, 1,4,8, 9,6,5,
+
+    7,2,6, 8,5,1, 4,9,3,
+    4,3,5, 9,7,6, 8,1,2,
+    1,9,8, 2,3,4, 7,5,6,
+];
 
 // From Lovatts sudoku book (issue 232, puzzle #53, 'super' difficulty).
 #[rustfmt::skip] 
-pub static INPUT1: [u8; 81] = [
+pub static INPUT2: [u8; 81] = [
     0, 0, 0,  0, 0, 0,  0, 4, 8,
     0, 0, 6,  0, 0, 9,  0, 0, 0,
     0, 0, 4,  0, 3, 0,  0, 0, 9,
@@ -17,7 +47,7 @@ pub static INPUT1: [u8; 81] = [
     2, 4, 0,  0, 0, 0,  0, 0, 0,
 ];
 #[rustfmt::skip]
-pub static OUTPUT1: [u8; 81]  = [
+pub static OUTPUT2: [u8; 81]  = [
     3, 9, 2,  5, 1, 7,  6, 4, 8,
     1, 5, 6,  8, 4, 9,  2, 3, 7,
     7, 8, 4,  6, 3, 2,  1, 5, 9,
@@ -33,7 +63,7 @@ pub static OUTPUT1: [u8; 81]  = [
 
 // From https://en.wikipedia.org/wiki/Sudoku
 #[rustfmt::skip]
-pub static INPUT2: [u8; 81] = [
+pub static INPUT1: [u8; 81] = [
     5, 3, 0,  0, 7, 0,  0, 0, 0,
     6, 0, 0,  1, 9, 5,  0, 0, 0,
     0, 9, 8,  0, 0, 0,  0, 6, 0,
@@ -47,7 +77,7 @@ pub static INPUT2: [u8; 81] = [
     0, 0, 0,  0, 8, 0,  0, 7, 9,
 ];
 #[rustfmt::skip]
-pub static OUTPUT2: [u8; 81]  = [
+pub static OUTPUT1: [u8; 81]  = [
     5, 3, 4,  6, 7, 8,  9, 1, 2,
     6, 7, 2,  1, 9, 5,  3, 4, 8,
     1, 9, 8,  3, 4, 2,  5, 6, 7,
