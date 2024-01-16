26402dda982d8be65de51d05e073a45985907e79
Campbell <campbell.wass@gmail.com>
2024-01-17 12:59:49 +1300

    Clean up benchmarks

--- crates/solver/benches/bench_main.rs
+++ crates/solver/benches/bench_main.rs
@@ -1,34 +1,26 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use data::*;
 use solver::*;
 
-fn test_solve(puzzle: [u8; 81]) {
+fn test_solve(puzzle: [u8; 81]) -> Board {
     let input = Board {
-        inner: black_box(puzzle),
+        inner: std::hint::black_box(puzzle),
     };
 
-    let board = solve(input, false);
-
-    black_box(&board);
+    solve(input, false)
 }
 
 pub fn bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("solve");
 
     group.bench_with_input("puzzle1", &INPUT1, |b, input| {
-        b.iter(|| {
-            black_box(test_solve(*input));
-        });
+        b.iter(|| test_solve(*input));
     });
     group.bench_with_input("puzzle2", &INPUT2, |b, input| {
-        b.iter(|| {
-            black_box(test_solve(*input));
-        });
+        b.iter(|| test_solve(*input));
     });
     group.bench_with_input("puzzle3", &INPUT3, |b, input| {
-        b.iter(|| {
-            black_box(test_solve(*input));
-        });
+        b.iter(|| test_solve(*input));
     });
 
     group.finish();
